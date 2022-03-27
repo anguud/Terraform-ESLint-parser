284ad270fa12e9d29ab34bdb57efde18141b5fee
Mr. Mikkelsen <bjmi@itu.dk>
2022-03-27 12:40:11 +0200

    works with TF, minor changes needed, parser2 is the woriking one.

--- momoa-master/thesis/main.js
+++ momoa-master/thesis/main.js
@@ -1,8 +1,21 @@
+// import { parse } from "../src/parse.js";
+import { tokenize } from "../src/tokens.js";
+import { getTokens } from "../../parser2/src/tokens.js";
+import { Parser } from "../../parser2/src/Parser.js";
 import { parse } from "../src/parse.js";
 
-const some_tf_string = '{resource "google_compute_ssl_policy" "vulnerable_example" { name = "production-ssl-policy";profile = "MODERN";min_tls_version = "TLS_1_0";}}'; 
-const some_json_string =  '{"fruit": "Apple", "size": "Large", "color": "Red"}'; 
+const some_tf_string = `{resource "google_compute_ssl_policy" "vulnerable_example" { 
+                                name = "production-ssl-policy"
+                                profile = "MODERN"
+                                min_tls_version = "TLS_1_0"
+                            }
+                        }`;
+const some_json_string = `{"fruit": 
+"Apple"
+, "size": "Large", "color": "Red", "thing": {"indetedThing": "test", "Anotherone": "yes"}}`; 
 
-const ast = parse(some_json_string);
+let pars = new Parser()
+const ast = pars.parse(some_tf_string);
+// const ast = getTokens(some_tf_string);
 
 console.log(JSON.stringify(ast,null, 2));
