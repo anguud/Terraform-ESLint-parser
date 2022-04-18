b89cbb139ebf52782b4ea6efde11af2b59f8218f
locm <locm@itu.dk>
2022-04-18 13:20:59 +0200

    added build folder and

---
+++ run-master/build/run-master/thesis/main.js
@@ -0,0 +1,13 @@
+"use strict";
+exports.__esModule = true;
+var Parser_1 = require("../../parserTS/src/Parser");
+//import { Parser } from "../../parserThesis/src/Parser";
+//import { parse } from "../../parserThesis/src/Parser.js";
+// import { parse } from "../src/parse.js";
+var some_tf_string = "resource \"google_compute_ssl_policy\" \"vulnerable_example\" { \n                                name = \"production-ssl-policy\"\n                                profile = \"MODERN\"\n                                min_tls_version = \"TLS_1_0\"\n                            }\n                        ";
+var test_string = "{ resource \"name\" \"name2\"}";
+var pars = new Parser_1.Parser();
+// const ast = parse(some_json_string);
+var ast = pars.parse(some_tf_string);
+// const ast = getTokens(some_tf_string);
+console.log(JSON.stringify(ast, null, 3));
