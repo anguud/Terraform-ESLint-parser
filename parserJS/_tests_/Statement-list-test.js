4e28bb6241c16dbba1ae406a7946447b6e6491b1
locm <locm@itu.dk>
2022-04-18 14:21:46 +0200

    added ParserJS, readonly

---
+++ parserJS/_tests_/Statement-list-test.js
@@ -0,0 +1,29 @@
+module.exports = test => {
+    test(
+        `
+        "Hello";
+
+        42;
+
+    `,
+        {
+            type: 'Program',
+            body: [
+              {
+                type: 'ExpressionStatement',
+                expression: {
+                  type: 'StringLiteral',
+                  value: 'Hello',
+                },
+              },
+              {
+                type: 'ExpressionStatement',
+                expression: {
+                  type: 'NumericLiteral',
+                  value: 42,
+                },
+              },
+            ],
+        },
+    );
+};
